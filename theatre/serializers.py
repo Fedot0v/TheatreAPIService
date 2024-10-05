8e5505f4945bc700174793c06b1171b8fac5f6b5
Mykyta Fedotov <nikita.fedotov222@gmail.com>
2024-10-05 12:36:16 +0300

    create performance list serializer with free seats count feature

--- theatre/serializers.py
+++ theatre/serializers.py
@@ -1,6 +1,14 @@
 from rest_framework import serializers
 
-from theatre.models import Actor, Genre, Play
+from theatre.models import (
+    Actor,
+    Genre,
+    Play,
+    TheatreHall,
+    Performance,
+    Reservation,
+    Ticket,
+)
 
 
 class ActorSerializer(serializers.ModelSerializer):
@@ -35,3 +43,52 @@ class GenreDetailSerializer(GenreSerializer):
         fields = GenreSerializer.Meta.fields + ("movies",)
 
 
+class PlayListSerializer(PlaySerializer):
+    class Meta(PlaySerializer.Meta):
+        fields = ("id", "title")
+
+
+class PlayDetailSerializer(PlaySerializer):
+    genres = GenreSerializer(many=True, read_only=True)
+    actors = ActorSerializer(many=True, read_only=True)
+
+
+class TheatreHallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TheatreHall
+        fields = ("id", "name", "rows", "seats_in_row")
+
+
+class PerformanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Performance
+        fields = ("id", "play", "theatre_hall", "show_time")
+
+
+class PerformanceListSerializer(PerformanceSerializer):
+    """We define a field to get the number of free seats through the serializer method"""
+    free_seats_count = serializers.SerializerMethodField()
+
+    """We call the get_free_seats_count() method of the Performance model to get it
+    of free places for the current object (obj) """
+    def get_available_seats_count(self, obj):
+        return obj.free_seats_count()
+
+
+class PerformanceDetailSerializer(PerformanceSerializer):
+    play = PlayDetailSerializer(many=True, read_only=True)
+    theatre_hall = TheatreHallSerializer(many=True, read_only=True)
+
+
+class ReservationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reservation
+        fields = ("id", "created_at", "user")
+        read_only_fields = ("created_at", "user")
+
+
+class TicketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = ("id", "row", "seat", "performance", "reservation")
+        read_only_fields = ("reservation",)
