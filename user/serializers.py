8f36a7871e6386561107a163ffdd9bed184578c4
Mykyta Fedotov <nikita.fedotov222@gmail.com>
2024-10-07 16:39:39 +0300

    fix serializers.ModelSerializer

--- user/serializers.py
+++ user/serializers.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ("id", "email", "password", "is_staff")
