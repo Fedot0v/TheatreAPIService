0aa60d63720f35b38cf9fbe3f86a1a128850e571
Mykyta Fedotov <nikita.fedotov222@gmail.com>
2024-10-13 13:53:48 +0300

    add schema path

--- TheatreAPIService/urls.py
+++ TheatreAPIService/urls.py
@@ -18,10 +18,17 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 from TheatreAPIService import settings
 
 urlpatterns = [
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        'api/schema/swagger-ui/',
+        SpectacularSwaggerView.as_view(url_name='schema'),
+        name='swagger-ui'
+    ),
     path('admin/', admin.site.urls),
     path("api/theatre/", include("theatre.urls", namespace="theatre")),
     path("api/user/", include("user.urls", namespace="user")),
