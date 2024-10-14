0505a4c2839f9ef572fab3766120337dbddf4ed6
Mykyta Fedotov <nikita.fedotov222@gmail.com>
2024-10-14 16:22:25 +0300

    add schema path

--- TheatreAPIService/urls.py
+++ TheatreAPIService/urls.py
@@ -25,11 +25,11 @@ from TheatreAPIService import settings
 urlpatterns = [
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        'api/schema/swagger-ui/',
-        SpectacularSwaggerView.as_view(url_name='schema'),
-        name='swagger-ui'
+        "api/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui"
     ),
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("api/theatre/", include("theatre.urls", namespace="theatre")),
     path("api/user/", include("user.urls", namespace="user")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
